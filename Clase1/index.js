adc2ebd938bebfa63b68cccec118104049feeb9e
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2024-01-15 22:45:07 -0300

    Add files via upload

---
+++ Clase1/index.js
@@ -0,0 +1,49 @@
+console.log("ESTO ES DEL SCRIPT EXTERNO");
+
+console.log(17 < 18); // true
+console.log(17 <= 18); // true
+console.log(18 <= 18); // true
+console.log(5 + 5); // 10
+console.log("5 + 5 = ", 5 + 5); // 10
+
+document.write("<h1>Bienvenido al modulo de JS</h1>");
+
+// BUSCAR DOS METODOS DEL OBJETO DOCUMENT EN JS
+
+// alert("HOLAAAAA BIENVENIDOOSSS")
+// alert("HOLAAAAA BIENVENIDOOSSS2")
+// alert("HOLAAAAA BIENVENIDOOSSS3")
+// TODO: CHE REVISAR ESTO
+// ESTO LO QUE HACE ES PEDIR SU NOMBRE AL USER Y MOSTRARLO POR CONSOLA
+console.log("Hola", prompt("Como te llamas?"));
+// TODO: BUG REVIEW
+
+// TODO: LINDA REVISA LA FUNCION DE ARRIBA
+// FIXME: HOLA
+
+// TODO: NO TOCAR LO DE ARRIBAM, NO SABEMOS QUE HACE PERO SI LO ELIMINAMOS SE ROMPE
+/*
+esto 
+es un
+comentario
+multi
+*/
+
+
+
+// VARIABLES MAL 
+var NO_USAR = "NO USAR ESTA FORMA"
+// BUG: Juan, que onda este var?  
+// Cambio por bug
+// VARIABLES BIEN
+let nombre = "Francisco"
+let camelCase = "Francisco"
+let PascalCase = "Francisco"
+let nombre_apellido = "Francisco"
+
+// CONSTANTES BIEN
+const DNI = 123456
+
+
+
+
